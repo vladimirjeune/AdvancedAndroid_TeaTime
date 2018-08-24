a9c84614471b480e0d0458b74f903c47ea711188
vladimirjeune <randyjeune@gmail.com>
2018-08-24 17:08:05 -0400

    feat: Create your first Espresso Test

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -16,13 +16,28 @@
 
 package com.example.android.teatime;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * This test demos a user clicking the decrement button and verifying that it properly decrease
  * the quantity the total cost.
  */
 
 // TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+    @RunWith(AndroidJUnit4.class)
 public class OrderActivityBasicTest {
+        @Rule public ActivityTestRule<OrderActivity> mActivityTestRule = new ActivityTestRule<>(OrderActivity.class);
 
     // TODO (2) Add the rule that provides functional testing of a single activity
 
@@ -31,6 +46,24 @@ public class OrderActivityBasicTest {
     //          - Click on the decrement button
     //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
 
+    @Test
     public void clickDecrementButton_ChangesQuantityAndCost() {
+
+        // Initially the cost is 0
+        onView(withId(R.id.quantity_text_view))
+                .check(matches(withText("0")));
+
+        // Decrement
+        onView(withId(R.id.decrement_button))
+                .perform(click());
+
+        // The cost amount should still be 0
+        onView(withId(R.id.quantity_text_view))
+                .check(matches(withText("0")));
+
+        // The cost should still be 0
+        onView(withId(R.id.cost_text_view))
+                .check(matches(withText("$0.00")));
+
     }
 }
